5885983fb8fe5dece88087196750467efcf556a4
Alexey Volkov <alexey.volkov@huawei.com>
2022-06-06 17:36:06 +0300

    more documentation

--- src/main/kotlin/org/utbot/java/compilation/database/api/ApiExt.kt
+++ src/main/kotlin/org/utbot/java/compilation/database/api/ApiExt.kt
@@ -3,51 +3,87 @@ package org.utbot.java.compilation.database.api
 import org.objectweb.asm.Opcodes
 import java.security.MessageDigest
 
+/**
+ * is item has `public` modifier
+ */
 suspend fun Accessible.isPublic(): Boolean {
     return access() and Opcodes.ACC_PUBLIC != 0
 }
 
+/**
+ * is item has `private` modifier
+ */
 suspend fun Accessible.isPrivate(): Boolean {
     return access() and Opcodes.ACC_PRIVATE != 0
 }
 
+/**
+ * is item has `protected` modifier
+ */
 suspend fun Accessible.isProtected(): Boolean {
     return access() and Opcodes.ACC_PROTECTED != 0
 }
 
+/**
+ * is item has `static` modifier
+ */
 suspend fun Accessible.isStatic(): Boolean {
     return access() and Opcodes.ACC_STATIC != 0
 }
 
+/**
+ * is item has `final` modifier
+ */
 suspend fun Accessible.isFinal(): Boolean {
     return access() and Opcodes.ACC_FINAL != 0
 }
 
-suspend fun Accessible.isSynchronized(): Boolean {
+/**
+ * is item has `synchronized` modifier
+ */
+suspend fun MethodId.isSynchronized(): Boolean {
     return access() and Opcodes.ACC_SYNCHRONIZED != 0
 }
 
-suspend fun Accessible.isVolatile(): Boolean {
+/**
+ * is item has `volatile` modifier
+ */
+suspend fun FieldId.isVolatile(): Boolean {
     return access() and Opcodes.ACC_VOLATILE != 0
 }
 
-suspend fun Accessible.isTransient(): Boolean {
+/**
+ * is field has `transient` modifier
+ */
+suspend fun FieldId.isTransient(): Boolean {
     return access() and Opcodes.ACC_TRANSIENT != 0
 }
 
-suspend fun Accessible.isNative(): Boolean {
+/**
+ * is method has `native` modifier
+ */
+suspend fun MethodId.isNative(): Boolean {
     return access() and Opcodes.ACC_NATIVE != 0
 }
 
-suspend fun Accessible.isInterface(): Boolean {
+/**
+ * is class is interface
+ */
+suspend fun ClassId.isInterface(): Boolean {
     return access() and Opcodes.ACC_INTERFACE != 0
 }
 
+/**
+ * is item has `abstract` modifier
+ */
 suspend fun Accessible.isAbstract(): Boolean {
     return access() and Opcodes.ACC_ABSTRACT != 0
 }
 
-suspend fun Accessible.isStrict(): Boolean {
+/**
+ * is method has `strictfp` modifier
+ */
+suspend fun MethodId.isStrict(): Boolean {
     return access() and Opcodes.ACC_STRICT != 0
 }
 
