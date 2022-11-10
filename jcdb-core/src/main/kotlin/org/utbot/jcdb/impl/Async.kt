e962453c34b7a9e390523e15dd47a996001af7f3
Alexey Volkov <lehvolk@gmail.com>
2022-11-10 11:28:11 +0300

    add ability to limit parallelism of background activity

--- jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/Async.kt
+++ jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/Async.kt
@@ -2,8 +2,17 @@ package org.utbot.jcdb.impl
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 
+val BACKGROUND_PARALLELISM
+    get() = Integer.getInteger(
+        "jcdb.background.parallelism",
+        64.coerceAtLeast(Runtime.getRuntime().availableProcessors())
+    )
+
 class BackgroundScope : CoroutineScope {
-    override val coroutineContext = Dispatchers.IO + SupervisorJob()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val coroutineContext = Dispatchers.IO.limitedParallelism(BACKGROUND_PARALLELISM) + SupervisorJob()
 }
