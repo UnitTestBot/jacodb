bd20d59c681038204ee1b85486057d2aeeed4c27
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-11-21 11:03:08 +0300

    less visitors

--- jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/cfg/Simplifier.kt
+++ jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/cfg/Simplifier.kt
@@ -7,105 +7,13 @@ import org.utbot.jcdb.impl.cfg.util.ExprMapper
 import org.utbot.jcdb.impl.cfg.util.FullExprSetCollector
 import org.utbot.jcdb.impl.cfg.util.InstructionFilter
 
-internal class UseCaseComputer : DefaultJcRawInstVisitor<Unit> {
-    val uses = mutableMapOf<JcRawSimpleValue, MutableSet<JcRawInst>>()
-    override val defaultInstHandler: (JcRawInst) -> Unit
-        get() = { inst ->
-            inst.operands
-                .flatMapTo(mutableSetOf()) { expr -> expr.applyAndGet(FullExprSetCollector()) { it.exprs } }
-                .filterIsInstance<JcRawSimpleValue>()
-                .filter { it !is JcRawConstant }
-                .forEach {
-                    uses.getOrPut(it, ::mutableSetOf).add(inst)
-                }
-        }
-
-    override fun visitJcRawAssignInst(inst: JcRawAssignInst) {
-        if (inst.lhv is JcRawComplexValue) {
-            inst.lhv.applyAndGet(FullExprSetCollector()) { it.exprs }
-                .filterIsInstance<JcRawSimpleValue>()
-                .filter { it !is JcRawConstant }
-                .forEach {
-                    uses.getOrPut(it, ::mutableSetOf).add(inst)
-                }
-        }
-        inst.rhv.applyAndGet(FullExprSetCollector()) { it.exprs }
-            .filterIsInstance<JcRawSimpleValue>()
-            .filter { it !is JcRawConstant }
-            .forEach {
-                uses.getOrPut(it, ::mutableSetOf).add(inst)
-            }
-    }
-
-    override fun visitJcRawCatchInst(inst: JcRawCatchInst) {}
-}
-
-internal class RepeatedAssignmentCleaner : DefaultJcRawInstVisitor<JcRawInst?> {
-    private val equalities = mutableMapOf<JcRawValue, JcRawValue>()
-    override val defaultInstHandler: (JcRawInst) -> JcRawInst?
-        get() = { it }
-
-    override fun visitJcRawAssignInst(inst: JcRawAssignInst): JcRawInst? {
-        val rhv = inst.rhv
-        if (rhv is JcRawValue) {
-            if (equalities[inst.lhv] == rhv) {
-                return null
-            } else {
-                equalities[inst.lhv] = rhv
-            }
-        }
-        return inst
-    }
-
-    override fun visitJcRawLabelInst(inst: JcRawLabelInst): JcRawInst {
-        equalities.clear()
-        return inst
-    }
-}
-
-internal class ReplacementComputer(private val uses: Map<JcRawValue, Set<JcRawInst>>) : DefaultJcRawInstVisitor<Unit> {
-    val replacements = mutableMapOf<JcRawRegister, JcRawValue>()
-    val reservedValues = mutableSetOf<JcRawValue>()
-    val replacedInsts = mutableSetOf<JcRawInst>()
-
-    override val defaultInstHandler: (JcRawInst) -> Unit
-        get() = { }
-
-    override fun visitJcRawAssignInst(inst: JcRawAssignInst) {
-        val rhv = inst.rhv
-        if (inst.lhv is JcRawSimpleValue
-            && rhv is JcRawRegister
-            && uses.getOrDefault(inst.rhv, emptySet()).firstOrNull() == inst
-            && rhv !in reservedValues
-        ) {
-            replacements[rhv] = inst.lhv
-            reservedValues += inst.lhv
-            replacedInsts += inst
-        }
-    }
-}
-
-internal class AssignmentSimplifier : DefaultJcRawInstVisitor<Unit> {
-    val assignments = mutableMapOf<JcRawSimpleValue, MutableSet<JcRawSimpleValue>>()
-
-    override val defaultInstHandler: (JcRawInst) -> Unit
-        get() = { }
-
-    override fun visitJcRawAssignInst(inst: JcRawAssignInst) {
-        val lhv = inst.lhv
-        val rhv = inst.rhv
-        if (lhv is JcRawRegister && rhv is JcRawRegister) {
-            assignments.getOrPut(lhv, ::mutableSetOf).add(rhv)
-        }
-    }
-}
-
 internal class Simplifier {
+
     fun simplify(instList: JcRawInstList): JcRawInstList {
-        var instructionList = instList.mapNotNull(RepeatedAssignmentCleaner())
+        var instructionList = cleanRepeatedAssignments(instList)
 
         do {
-            val uses = instructionList.applyAndGet(UseCaseComputer()) { it.uses }
+            val uses = computeUseCases(instructionList)
             val oldSize = instructionList.instructions.size
             instructionList = instructionList.filterNot(InstructionFilter {
                 it is JcRawAssignInst
@@ -116,8 +24,8 @@ internal class Simplifier {
         } while (instructionList.instructions.size != oldSize)
 
         do {
-            val uses = instructionList.applyAndGet(AssignmentSimplifier()) { it.assignments }
-            val replacements = uses.filterValues { it.size == 1 }.map { it.key to it.value.first() }.toMap()
+            val assignmentsMap = computeAssignments(instructionList)
+            val replacements = assignmentsMap.filterValues { it.size == 1 }.map { it.key to it.value.first() }.toMap()
             instructionList = instructionList
                 .filterNot(InstructionFilter {
                     if (it !is JcRawAssignInst) return@InstructionFilter false
@@ -132,15 +40,117 @@ internal class Simplifier {
         } while (replacements.isNotEmpty())
 
         do {
-            val uses = instructionList.applyAndGet(UseCaseComputer()) { it.uses }
-            val (replacements, instructionsToDelete) = instructionList.applyAndGet(ReplacementComputer(uses.toMap())) {
-                it.replacements to it.replacedInsts
-            }
+            val uses = computeUseCases(instructionList)
+            val (replacements, instructionsToDelete) = computeReplacements(instructionList, uses)
             instructionList = instructionList
                 .map(ExprMapper(replacements.toMap()))
                 .filter(InstructionFilter { it !in instructionsToDelete })
         } while (replacements.isNotEmpty())
 
         return instructionList
+    }
+
+
+
+    private fun computeUseCases(instList: JcRawInstList): Map<JcRawSimpleValue, Set<JcRawInst>> {
+        val uses = mutableMapOf<JcRawSimpleValue, MutableSet<JcRawInst>>()
+        for (inst in instList) {
+            when (inst) {
+                is JcRawAssignInst -> {
+                    if (inst.lhv is JcRawComplexValue) {
+                        inst.lhv.applyAndGet(FullExprSetCollector()) { it.exprs }
+                            .filterIsInstance<JcRawSimpleValue>()
+                            .filter { it !is JcRawConstant }
+                            .forEach {
+                                uses.getOrPut(it, ::mutableSetOf).add(inst)
+                            }
+                    }
+                    inst.rhv.applyAndGet(FullExprSetCollector()) { it.exprs }
+                        .filterIsInstance<JcRawSimpleValue>()
+                        .filter { it !is JcRawConstant }
+                        .forEach {
+                            uses.getOrPut(it, ::mutableSetOf).add(inst)
+                        }
+                }
+                is JcRawCatchInst -> {}
+                else -> {
+                    inst.operands
+                        .flatMapTo(mutableSetOf()) { expr -> expr.applyAndGet(FullExprSetCollector()) { it.exprs } }
+                        .filterIsInstance<JcRawSimpleValue>()
+                        .filter { it !is JcRawConstant }
+                        .forEach {
+                            uses.getOrPut(it, ::mutableSetOf).add(inst)
+                        }
+                }
+            }
+        }
+        return uses
+    }
+
+    private fun cleanRepeatedAssignments(instList: JcRawInstList): JcRawInstList {
+        val instructions = mutableListOf<JcRawInst>()
+        val equalities = mutableMapOf<JcRawSimpleValue, JcRawSimpleValue>()
+        for (inst in instList) {
+            when (inst) {
+                is JcRawAssignInst -> {
+                    val lhv = inst.lhv
+                    val rhv = inst.rhv
+                    if (lhv is JcRawSimpleValue && rhv is JcRawSimpleValue) {
+                        if (equalities[lhv] != rhv) {
+                            equalities[lhv] = rhv
+                            instructions += inst
+                        }
+                    } else {
+                        instructions += inst
+                    }
+                }
+                is JcRawLabelInst -> {
+                    instructions += inst
+                    equalities.clear()
+                }
+                else -> instructions += inst
+            }
+        }
+        return JcRawInstList(instructions)
+    }
+
+    private fun computeReplacements(
+        instList: JcRawInstList,
+        uses: Map<JcRawSimpleValue, Set<JcRawInst>>
+    ): Pair<Map<JcRawRegister, JcRawValue>, Set<JcRawInst>> {
+        val replacements = mutableMapOf<JcRawRegister, JcRawValue>()
+        val reservedValues = mutableSetOf<JcRawValue>()
+        val replacedInsts = mutableSetOf<JcRawInst>()
+
+        for (inst in instList) {
+            if (inst is JcRawAssignInst) {
+                val rhv = inst.rhv
+                if (inst.lhv is JcRawSimpleValue
+                    && rhv is JcRawRegister
+                    && uses.getOrDefault(inst.rhv, emptySet()).firstOrNull() == inst
+                    && rhv !in reservedValues
+                ) {
+                    replacements[rhv] = inst.lhv
+                    reservedValues += inst.lhv
+                    replacedInsts += inst
+                }
+            }
+        }
+
+        return replacements to replacedInsts
+    }
+
+    private fun computeAssignments(instList: JcRawInstList): Map<JcRawSimpleValue, Set<JcRawSimpleValue>> {
+        val assignments = mutableMapOf<JcRawSimpleValue, MutableSet<JcRawSimpleValue>>()
+        for (inst in instList) {
+            if (inst is JcRawAssignInst) {
+                val lhv = inst.lhv
+                val rhv = inst.rhv
+                if (lhv is JcRawRegister && rhv is JcRawRegister) {
+                    assignments.getOrPut(lhv, ::mutableSetOf).add(rhv)
+                }
+            }
+        }
+        return assignments
     }
 }
