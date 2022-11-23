df666201c98d6165b93c06ff4b73b69c41785db6
Alexey Volkov <lehvolk@gmail.com>
2022-11-23 21:46:00 +0300

    try to fix ci tests

--- jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/storage/SQLitePersistenceImpl.kt
+++ jcdb-core/src/main/kotlin/org/utbot/jcdb/impl/storage/SQLitePersistenceImpl.kt
@@ -3,7 +3,6 @@ package org.utbot.jcdb.impl.storage
 import mu.KLogging
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
-import org.jooq.conf.Settings
 import org.jooq.impl.DSL
 import org.sqlite.SQLiteConfig
 import org.sqlite.SQLiteDataSource
@@ -64,7 +63,7 @@ class SQLitePersistenceImpl(
                 it.url = url
             }
         }
-        jooq = DSL.using(dataSource, SQLDialect.SQLITE, Settings().withExecuteLogging(false))
+        jooq = DSL.using(dataSource, SQLDialect.SQLITE)
         write {
             if (clearOnStart) {
                 jooq.executeQueriesFrom("jcdb-drop-schema.sql")
