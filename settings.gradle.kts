d5261f23ca30f799a7606effb4f2c7d2085e9be4
Alexey Volkov <lehvolk@gmail.com>
2022-12-09 17:26:46 +0300

    switch to gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
 rootProject.name = "jcdb"
 
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.25"
+}
+
+gitHooks {
+    preCommit {
+        // Content can be added at the bottom of the script
+        from(file("pre-commit").toURI().toURL())
+    }
+    createHooks() // actual hooks creation
+}
+
 include("jcdb-api")
 include("jcdb-core")
 include("jcdb-testing")
