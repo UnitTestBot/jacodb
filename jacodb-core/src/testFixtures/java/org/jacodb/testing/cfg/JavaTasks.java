4eb0af40397f7b7a93ab3fe7fb4c8c23e6b82b73
Alexey Volkov <lehvolk@gmail.com>
2023-02-02 12:53:54 +0300

    fix tests

--- jacodb-core/src/testFixtures/java/org/jacodb/testing/cfg/JavaTasks.java
+++ jacodb-core/src/testFixtures/java/org/jacodb/testing/cfg/JavaTasks.java
@@ -18,7 +18,11 @@ package org.jacodb.testing.cfg;
 
 import kotlin.NotImplementedError;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
