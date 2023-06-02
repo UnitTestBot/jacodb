d12ecfcf53cc7f499ea8a108751056d4b81617ab
Alexey Volkov <lehvolk@gmail.com>
2023-06-02 17:21:05 +0300

    bootstrap tests a bit

--- jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/BaseTest.kt
+++ jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/BaseTest.kt
@@ -60,6 +60,10 @@ open class WithDB(vararg features: JcFeature<*, *>) {
 
     protected var allFeatures = features.toList().toTypedArray()
 
+    init {
+        System.setProperty("org.jacodb.impl.storage.defaultBatchSize", "500")
+    }
+
     open var db = runBlocking {
         jacodb {
 //            persistent("D:\\work\\jacodb\\jcdb-index.db")
