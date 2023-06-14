7a2b93c775dd64fba501e1244f3cdb7ed2977f83
Ivan Volkov <65076429+volivan239@users.noreply.github.com>
2023-06-14 19:20:12 +0300

    Summaries support in ifds (#103)
    * [jacodb-ifds] First good results with summary-based ifds
    
    * [jacodb-ifds] Fix construction of RealisationGraphs
    
    * [jacodb-ifds] Reduce clutter
    
    * [jacodb-ifds] Refactoring and minor bug fixes

--- jacodb-analysis/src/test/kotlin/org/jacodb/analysis/impl/NpeAnalysisTest.kt
+++ jacodb-analysis/src/test/kotlin/org/jacodb/analysis/impl/NpeAnalysisTest.kt
@@ -16,8 +16,9 @@
 
 package org.jacodb.analysis.impl
 
+import juliet.testcasesupport.AbstractTestCase
 import kotlinx.coroutines.runBlocking
-import org.jacodb.analysis.AnalysisEngine
+import org.jacodb.analysis.DumpableVulnerabilityInstance
 import org.jacodb.analysis.JcNaivePoints2EngineFactory
 import org.jacodb.analysis.JcSimplifiedGraphFactory
 import org.jacodb.analysis.analyzers.NpeAnalyzer
@@ -25,9 +26,18 @@ import org.jacodb.analysis.engine.BidiIFDSForTaintAnalysis
 import org.jacodb.analysis.engine.IFDSUnitTraverser
 import org.jacodb.analysis.engine.SingletonUnitResolver
 import org.jacodb.analysis.graph.JcApplicationGraphImpl
+import org.jacodb.api.JcClassOrInterface
+import org.jacodb.api.JcMethod
 import org.jacodb.api.ext.constructors
 import org.jacodb.api.ext.findClass
+import org.jacodb.api.ext.methods
+import org.jacodb.impl.features.InMemoryHierarchy
+import org.jacodb.impl.features.Usages
+import org.jacodb.impl.features.hierarchyExt
 import org.jacodb.impl.features.usagesExt
+import org.jacodb.testing.BaseTest
+import org.jacodb.testing.WithDB
+import org.jacodb.testing.allClasspath
 import org.jacodb.testing.analysis.NPEExamples
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -36,18 +46,58 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
 import java.util.stream.Stream
+import kotlin.streams.asStream
 
-class NpeAnalysisTest : BaseAnalysisTest() {
-    companion object {
+class NpeAnalysisTest : BaseTest() {
+    companion object : WithDB(Usages, InMemoryHierarchy) {
         @JvmStatic
-        fun provideClassesForJuliet476(): Stream<Arguments> =
-            provideClassesForJuliet(476, listOf("null_check_after_deref"))
+        fun provideClassesForJuliet476(): Stream<Arguments> = runBlocking {
+            val cp = db.classpath(allClasspath)
+            val hierarchyExt = cp.hierarchyExt()
+            val baseClass = cp.findClass<AbstractTestCase>()
+            val classes = hierarchyExt.findSubClasses(baseClass, false)
+            classes.toArguments("CWE476")
+        }
 
         @JvmStatic
-        fun provideClassesForJuliet690(): Stream<Arguments> =
-            provideClassesForJuliet(690)
+        fun provideClassesForJuliet690(): Stream<Arguments> = runBlocking {
+            val cp = db.classpath(allClasspath)
+            val hierarchyExt = cp.hierarchyExt()
+            val baseClass = cp.findClass<AbstractTestCase>()
+            val classes = hierarchyExt.findSubClasses(baseClass, false)
+            classes.toArguments("CWE690")
+        }
 
-        private val vulnerabilityType = NpeAnalyzer.value
+        private fun Sequence<JcClassOrInterface>.toArguments(cwe: String): Stream<Arguments> = map { it.name }
+            .filter { it.contains(cwe) }
+            .filterNot { className -> bannedTests.any { className.contains(it) } }
+//            .filter { it.contains("Integer_68a") }
+            .sorted()
+            .map { Arguments.of(it) }
+            .asStream()
+
+        private val bannedTests = listOf(
+            // not NPE problems
+            "null_check_after_deref",
+
+            // TODO: containers not supported
+            "_72", "_73", "_74",
+
+            // TODO/Won't fix(?): dead parts of switches shouldn't be analyzed
+            "_15",
+
+            // TODO/Won't fix(?): passing through channels not supported
+            "_75",
+
+            // TODO/Won't fix(?): constant private/static methods not analyzed
+            "_11", "_08",
+
+            // TODO/Won't fix(?): unmodified non-final private variables not analyzed
+            "_05", "_07",
+
+            // TODO/Won't fix(?): unmodified non-final static variables not analyzed
+            "_10", "_14"
+        )
     }
 
     @Test
@@ -192,6 +242,26 @@ class NpeAnalysisTest : BaseAnalysisTest() {
         testJuliet(className)
     }
 
+    private fun testJuliet(className: String) {
+        val clazz = cp.findClass(className)
+        val goodMethod = clazz.methods.single { it.name == "good" }
+        val badMethod = clazz.methods.single { it.name == "bad" }
+
+        val goodNPE = findNpeSources(goodMethod)
+        val badNPE = findNpeSources(badMethod)
+
+        assertTrue(goodNPE.isEmpty())
+        assertTrue(badNPE.isNotEmpty())
+    }
+
+    private inline fun <reified T> testOneMethod(methodName: String, expectedLocations: Collection<String>) {
+        val method = cp.findClass<T>().declaredMethods.single { it.name == methodName }
+        val npes = findNpeSources(method)
+
+        // TODO: think about better assertions here
+        assertEquals(expectedLocations.toSet(), npes.map { it.sink }.toSet())
+    }
+
     @Test
     fun `analyse something`() {
         val testingMethod = cp.findClass<NPEExamples>().declaredMethods.single { it.name == "id" }
@@ -199,22 +269,12 @@ class NpeAnalysisTest : BaseAnalysisTest() {
         print(results)
     }
 
-    private fun testJuliet(className: String) = testSingleJulietClass(engine, vulnerabilityType, className)
-
-    private inline fun <reified T> testOneMethod(methodName: String, expectedLocations: Collection<String>) =
-        testOneAnalysisOnOneMethod<T>(engine, vulnerabilityType, methodName, expectedLocations)
-
-    private val engine: AnalysisEngine
-        get() {
-            val graph = JcSimplifiedGraphFactory().createGraph(cp)
-            val points2Engine = JcNaivePoints2EngineFactory.createPoints2Engine(graph)
-
-            return IFDSUnitTraverser(
-                graph,
-                NpeAnalyzer(graph),
-                SingletonUnitResolver,
-                points2Engine.obtainDevirtualizer(),
-                BidiIFDSForTaintAnalysis
-            )
-        }
+    private fun findNpeSources(method: JcMethod): List<DumpableVulnerabilityInstance> {
+        val graph = JcSimplifiedGraphFactory().createGraph(cp)
+        val points2Engine = JcNaivePoints2EngineFactory.createPoints2Engine(graph)
+        val ifds = IFDSUnitTraverser(graph, NpeAnalyzer(graph), SingletonUnitResolver, points2Engine.obtainDevirtualizer(), BidiIFDSForTaintAnalysis)
+        ifds.addStart(method)
+        val result = ifds.analyze().toDumpable()
+        return result.foundVulnerabilities.filter { it.vulnerabilityType == NpeAnalyzer.value }
+    }
 }
