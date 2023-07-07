31828db8f07e5126cc786c7483cb8f802761cd73
Alexey Volkov <lehvolk@gmail.com>
2023-07-07 15:30:49 +0300

    add CaelmBleidd to contributors

--- build.gradle.kts
+++ build.gradle.kts
@@ -269,6 +269,11 @@ fun MavenPublication.addPom() {
                 email.set("azat.aam@gmail.com")
             }
             developer {
+                id.set("CaelmBleidd")
+                name.set("Alexey Menshutin")
+                email.set("alex.menshutin99@gmail.com")
+            }
+            developer {
                 id.set("sergeypospelov")
                 name.set("Sergey Pospelov")
                 email.set("sergeypospelov59@gmail.com")
