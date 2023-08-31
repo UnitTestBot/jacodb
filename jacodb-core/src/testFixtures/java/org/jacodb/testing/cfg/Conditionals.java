b78fdbc881c98c03c297630ccca68edb729f73d5
DaniilStepanov <stepanov0995@mail.ru>
2023-08-31 16:01:58 +0300

    Bug with if in condition found

--- jacodb-core/src/testFixtures/java/org/jacodb/testing/cfg/Conditionals.java
+++ jacodb-core/src/testFixtures/java/org/jacodb/testing/cfg/Conditionals.java
@@ -16,6 +16,11 @@
 
 package org.jacodb.testing.cfg;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Conditionals {
     void main(int x, int y) {
         if (x < 0) {
@@ -31,4 +36,26 @@ public class Conditionals {
             System.out.println("<=");
         }
     }
+
+    public static void conditionInFor() {
+        Random rnd = new Random();
+        List<Boolean> list = new ArrayList<>();
+        int numFalse = 0;
+        for (int i = 0; i < 1000; i++) {
+            boolean element = rnd.nextBoolean();
+            if (!element)
+                numFalse++;
+            list.add(element);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < numFalse; i++)
+            if (list.get(i))
+                throw new RuntimeException("False positive: " + i);
+        for (int i = numFalse; i < 1000; i++)
+            if (!list.get(i))
+                throw new RuntimeException("False negative: " + i);
+    }
+
 }
