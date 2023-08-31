b98624ff57f7fcbb28940e2fca3168a2e57f0107
Alexey Volkov <lehvolk@gmail.com>
2023-08-31 18:34:01 +0300

    Wrong lambdas conversion from Jacodb IR to JVM bytecode #160

--- jacodb-core/src/main/kotlin/org/jacodb/impl/cfg/MethodNodeBuilder.kt
+++ jacodb-core/src/main/kotlin/org/jacodb/impl/cfg/MethodNodeBuilder.kt
@@ -585,7 +585,7 @@ class MethodNodeBuilder(
                         is BsmHandle -> it.asAsmHandle
                         else -> error("Unknown arg of bsm: $it")
                     }
-                }.toTypedArray()
+                }.reversed().toTypedArray()
             )
         )
         updateStackInfo(-expr.args.size + 1)
