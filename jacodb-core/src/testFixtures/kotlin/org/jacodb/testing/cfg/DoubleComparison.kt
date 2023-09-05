29e30b0a3f11aac80572b408b905c07773aa0ea5
DaniilStepanov <stepanov0995@mail.ru>
2023-09-05 16:36:49 +0300

    New tests and some fixes

--- jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/cfg/DoubleComparison.kt
+++ jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/cfg/DoubleComparison.kt
@@ -16,27 +16,9 @@
 
 package org.jacodb.testing.cfg
 
-import org.junit.jupiter.api.Assertions.assertEquals
-
-
-class Varargs {
-
-
-    fun <T> foo(vararg a: T) = a.size
-
-    inline fun <reified T> bar(block: () -> T): Array<T> {
-        assertEquals(2, foo(block(), block()))
-
-        return arrayOf(block(), block(), block())
-    }
-
-    inline fun <reified T> empty() = arrayOf<T>()
-
+class DoubleComparison {
     fun box(): String {
-        var i = 0
-        val a: Array<String> = bar() { i++; i.toString() }
-        assertEquals("345", a.joinToString(""))
+        if ((-0.0 as Comparable<Double>) >= 0.0) return "fail"
         return "OK"
     }
-
 }
--- jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/cfg/TryCatchFinally.kt
+++ jacodb-core/src/testFixtures/kotlin/org/jacodb/testing/cfg/TryCatchFinally.kt
@@ -16,26 +16,22 @@
 
 package org.jacodb.testing.cfg
 
-import org.junit.jupiter.api.Assertions.assertEquals
+class TryCatchFinally {
 
-
-class Varargs {
-
-
-    fun <T> foo(vararg a: T) = a.size
-
-    inline fun <reified T> bar(block: () -> T): Array<T> {
-        assertEquals(2, foo(block(), block()))
-
-        return arrayOf(block(), block(), block())
+    fun f(): Int {
+        try {
+            return 0
+        }
+        finally {
+            try { // culprit ?? remove this try-catch and it works.
+            } catch (ignore: Exception) {
+            }
+        }
     }
 
-    inline fun <reified T> empty() = arrayOf<T>()
-
     fun box(): String {
-        var i = 0
-        val a: Array<String> = bar() { i++; i.toString() }
-        assertEquals("345", a.joinToString(""))
+        if (f() != 0)  return "fail1"
+
         return "OK"
     }
 
