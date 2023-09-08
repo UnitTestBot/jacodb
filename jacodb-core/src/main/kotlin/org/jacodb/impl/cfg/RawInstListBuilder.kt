573cc3ac8c7bd8a05bf06eb8482153ae4369041e
DaniilStepanov <stepanov0995@mail.ru>
2023-09-08 17:56:35 +0300

    possible fix for TOP type bugs

--- jacodb-core/src/main/kotlin/org/jacodb/impl/cfg/RawInstListBuilder.kt
+++ jacodb-core/src/main/kotlin/org/jacodb/impl/cfg/RawInstListBuilder.kt
@@ -191,6 +191,8 @@ class RawInstListBuilder(
     private val laterAssignments = identityMap<AbstractInsnNode, MutableMap<Int, JcRawValue>>()
     private val laterStackAssignments = identityMap<AbstractInsnNode, MutableMap<Int, JcRawValue>>()
     private val localTypeRefinement = identityMap<JcRawLocalVar, JcRawLocalVar>()
+    private val blackListForTypeRefinement = listOf(TOP, NULL, UNINIT_THIS)
+    private val postfixInstructions = hashMapOf<Int, JcRawInst>()
 
     private var labelCounter = 0
     private var localCounter = 0
@@ -877,7 +879,7 @@ class RawInstListBuilder(
 
                     else -> frame.locals.filterKeys { it in this }.mapValues {
                         when {
-                            it.value is JcRawLocalVar && it.value.typeName != this[it.key]!! && this[it.key] != TOP -> JcRawLocalVar(
+                            it.value is JcRawLocalVar && it.value.typeName != this[it.key]!! && this[it.key] !in blackListForTypeRefinement -> JcRawLocalVar(
                                 (it.value as JcRawLocalVar).name,
                                 this[it.key]!!
                             ).also { newLocal ->
@@ -951,7 +953,7 @@ class RawInstListBuilder(
 
                 else -> frame.stack.withIndex().filter { it.index in this }.map {
                     when {
-                        it.value is JcRawLocalVar && it.value.typeName != this[it.index]!! && this[it.index] != TOP -> JcRawLocalVar(
+                        it.value is JcRawLocalVar && it.value.typeName != this[it.index]!! && this[it.index] !in blackListForTypeRefinement -> JcRawLocalVar(
                             (it.value as JcRawLocalVar).name,
                             this[it.index]!!
                         ).also { newLocal ->
