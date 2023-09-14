f9e93e4ab161a9ba4a17f9b235aa38712fd3a48b
Alexey Volkov <lehvolk@gmail.com>
2023-09-14 17:13:37 +0300

    speedup tests

--- jacodb-core/src/test/kotlin/org/jacodb/testing/persistence/IncompleteDataTest.kt
+++ jacodb-core/src/test/kotlin/org/jacodb/testing/persistence/IncompleteDataTest.kt
@@ -30,7 +30,6 @@ import org.jacodb.testing.allClasspath
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.nio.file.Files
@@ -38,15 +37,23 @@ import java.nio.file.Files
 
 class IncompleteDataTest {
 
-    private val jdbcLocation = Files.createTempFile("jcdb-", null).toFile().absolutePath
-    private lateinit var javaHome: File
+    companion object {
+        private val jdbcLocation = Files.createTempFile("jcdb-", null).toFile().absolutePath
+        private val javaHome: File = JcSettings().useProcessJavaRuntime().jre
+        val db = newDB(true).also {
+            it.close()
+        }
 
-    @BeforeEach
-    fun setupDB() {
-        javaHome = JcSettings().useProcessJavaRuntime().jre
-        runBlocking {
-            newDB(true).also {
-                it.close()
+        private fun newDB(awaitBackground: Boolean) = runBlocking {
+            jacodb {
+                useProcessJavaRuntime()
+                persistent(jdbcLocation)
+                installFeatures(Usages, Builders)
+                loadByteCode(allClasspath)
+            }.also {
+                if (awaitBackground) {
+                    it.awaitBackgroundJobs()
+                }
             }
         }
     }
@@ -104,19 +111,6 @@ class IncompleteDataTest {
         persistence.use {
             it.write {
                 action(it)
-            }
-        }
-    }
-
-    private fun newDB(awaitBackground: Boolean) = runBlocking {
-        jacodb {
-            useProcessJavaRuntime()
-            persistent(jdbcLocation)
-            installFeatures(Usages, Builders)
-            loadByteCode(allClasspath)
-        }.also {
-            if (awaitBackground) {
-                it.awaitBackgroundJobs()
             }
         }
     }
