099fc2613967b9c65329021ff7f60639b7b415f6
Alexey Volkov <lehvolk@gmail.com>
2023-10-09 11:23:08 +0300

    optimize and cleanup tests

--- jacodb-core/src/test/kotlin/org/jacodb/testing/cfg/KotlinInstructionsTest.kt
+++ jacodb-core/src/test/kotlin/org/jacodb/testing/cfg/KotlinInstructionsTest.kt
@@ -16,63 +16,60 @@
 
 package org.jacodb.testing.cfg
 
-import org.jacodb.testing.WithDB
 import org.junit.jupiter.api.Test
 
 class KotlinInstructionsTest: BaseInstructionsTest() {
 
-    companion object : WithDB()
+    @Test
+    fun `simple test`() = runTest(SimpleTest::class.java.name)
 
     @Test
-    fun `simple test`() = runKotlinTest(SimpleTest::class.java.name)
+    fun `kotlin vararg test`() = runTest(Varargs::class.java.name)
 
     @Test
-    fun `kotlin vararg test`() = runKotlinTest(Varargs::class.java.name)
+    fun `kotlin equals test`() = runTest(Equals::class.java.name)
 
     @Test
-    fun `kotlin equals test`() = runKotlinTest(Equals::class.java.name)
+    fun `kotlin different receivers test`() = runTest(DifferentReceivers::class.java.name)
 
     @Test
-    fun `kotlin different receivers test`() = runKotlinTest(DifferentReceivers::class.java.name)
+    fun `kotlin sequence test`() = runTest(KotlinSequence::class.java.name)
 
     @Test
-    fun `kotlin sequence test`() = runKotlinTest(KotlinSequence::class.java.name)
+    fun `kotlin range test`() = runTest(Ranges::class.java.name)
 
     @Test
-    fun `kotlin range test`() = runKotlinTest(Ranges::class.java.name)
-
-    @Test
-    fun `kotlin range test 2`() = runKotlinTest(Ranges2::class.java.name)
+    fun `kotlin range test 2`() = runTest(Ranges2::class.java.name)
 
 //    @Test
 //    fun `kotlin overloading test`() = runKotlinTest(Overloading::class.java.name)
 
     //We have to mute graph checker because of empty catch-es in try/catch blocks
     @Test
-    fun `kotlin try catch finally`() = runKotlinTest(TryCatchFinally::class.java.name, muteGraphChecker = true)
+    fun `kotlin try catch finally`() = runTest(TryCatchFinally::class.java.name, muteGraphChecker = true)
 
     @Test
-    fun `kotlin try catch finally 2`() = runKotlinTest(TryCatchFinally2::class.java.name, muteGraphChecker = true)
+    fun `kotlin try catch finally 2`() = runTest(TryCatchFinally2::class.java.name, muteGraphChecker = true)
 
     @Test
-    fun `kotlin try catch finally 3`() = runKotlinTest(TryCatchFinally3::class.java.name, muteGraphChecker = true)
+    fun `kotlin try catch finally 3`() = runTest(TryCatchFinally3::class.java.name, muteGraphChecker = true)
 
     @Test
-    fun `kotlin try catch finally 4`() = runKotlinTest(TryCatchFinally4::class.java.name, muteGraphChecker = true)
+    fun `kotlin try catch finally 4`() = runTest(TryCatchFinally4::class.java.name, muteGraphChecker = true)
 
     @Test
-    fun `kotlin method with exception`() = runKotlinTest(InvokeMethodWithException::class.java.name)
+    fun `kotlin method with exception`() = runTest(InvokeMethodWithException::class.java.name)
 
     @Test
-    fun `kotlin typecast`() = runKotlinTest(DoubleComparison::class.java.name)
+    fun `kotlin typecast`() = runTest(DoubleComparison::class.java.name)
 
     @Test
-    fun `kotlin when expr`() = runKotlinTest(WhenExpr::class.java.name)
+    fun `kotlin when expr`() = runTest(WhenExpr::class.java.name)
 
     @Test
-    fun `kotlin default args`() = runKotlinTest(DefaultArgs::class.java.name)
+    fun `kotlin default args`() = runTest(DefaultArgs::class.java.name)
 
     @Test
-    fun `kotlin arrays`() = runKotlinTest(Arrays::class.java.name)
+    fun `kotlin arrays`() = runTest(Arrays::class.java.name)
 
 }
