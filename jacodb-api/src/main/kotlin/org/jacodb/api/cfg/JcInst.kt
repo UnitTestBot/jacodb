00e2a3d0e314592ed345930423f8becab9a8dff3
DaniilStepanov <stepanov0995@mail.ru>
2023-11-21 15:18:15 +0300

    Stepanov/multi dim array fix (#202)
    * Creation of multidimensional arrays fixed
    * minor
    * Rewrite toString() method for JcNewArray and JcRawNewArray

--- jacodb-api/src/main/kotlin/org/jacodb/api/cfg/JcInst.kt
+++ jacodb-api/src/main/kotlin/org/jacodb/api/cfg/JcInst.kt
@@ -606,10 +606,21 @@ data class JcNewArrayExpr(
     override val operands: List<JcValue>
         get() = dimensions
 
-    override fun toString(): String = "new ${type.typeName}${dimensions.joinToString("") { "[$it]" }}"
+    override fun toString(): String = "new ${arrayTypeToStringWithDimensions(type.typeName, dimensions)}"
 
     override fun <T> accept(visitor: JcExprVisitor<T>): T {
         return visitor.visitJcNewArrayExpr(this)
+    }
+
+    companion object {
+        private val regexToProcessDimensions = Regex("\\[(.*?)]")
+
+        private fun arrayTypeToStringWithDimensions(typeName: String, dimensions: List<JcValue>) {
+            var curDim = 0
+            regexToProcessDimensions.replace(typeName) {
+                "[${dimensions.getOrNull(curDim++) ?: ""}]"
+            }
+        }
     }
 }
 
