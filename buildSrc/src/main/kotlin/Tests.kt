42726a38533520b6525c3eebabf5a0c0166680a4
Konstantin Chukharev <lipen00@gmail.com>
2023-12-05 17:49:16 +0300

    FULL exceptions in tests

--- buildSrc/src/main/kotlin/Tests.kt
+++ buildSrc/src/main/kotlin/Tests.kt
@@ -1,5 +1,6 @@
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 object Tests {
     val lifecycleTag = "lifecycle"
@@ -9,6 +10,7 @@ object Tests {
 fun Test.setup(jacocoTestReport: TaskProvider<*>) {
     testLogging {
         events("passed", "skipped", "failed")
+        exceptionFormat = TestExceptionFormat.FULL
     }
     finalizedBy(jacocoTestReport) // report is always generated after tests run
     jvmArgs = listOf("-Xmx2g", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:HeapDumpPath=heapdump.hprof")
