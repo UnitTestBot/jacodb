d11afbc949e2c6bed1d00a22a875522f69f81e5d
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2024-01-30 15:52:36 +0300

    Init panda-dynamic module

---
+++ jacodb-panda-dynamic/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "org.jacodb"
+version = "1.4-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
