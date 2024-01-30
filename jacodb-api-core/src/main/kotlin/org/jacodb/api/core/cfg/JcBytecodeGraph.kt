8083c50c846d869002093c188b3c925437de14f8
Alexey Menshutin <alex.menshutin99@gmail.com>
2024-01-30 18:25:13 +0300

    Add core

---
+++ jacodb-api-core/src/main/kotlin/org/jacodb/api/core/cfg/JcBytecodeGraph.kt
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2022 UnitTestBot contributors (utbot.org)
+ * <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.jacodb.api.core.cfg
+
+interface Graph<NODE> : Iterable<NODE> {
+    fun successors(node: NODE): Set<NODE>
+    fun predecessors(node: NODE): Set<NODE>
+}
+
+// TODO Caelmbleidd rename it?
+interface ControlFlowGraph<NODE> : Graph<NODE> {
+    val entries: List<NODE>
+    val exits: List<NODE>
+
+    val instructions: List<NODE>
+}
