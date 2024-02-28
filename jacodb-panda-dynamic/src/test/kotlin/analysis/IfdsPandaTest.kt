cbc3aa2182475f8600dac464a06d02c85109b41b
Konstantin Chukharev <lipen00@gmail.com>
2024-02-28 12:55:52 +0300

    Update panda-analysis tests infrastructure

--- jacodb-panda-dynamic/src/test/kotlin/analysis/IfdsPandaTest.kt
+++ jacodb-panda-dynamic/src/test/kotlin/analysis/IfdsPandaTest.kt
@@ -19,26 +19,33 @@ package analysis
 import io.mockk.mockk
 import org.jacodb.analysis.ifds.SingletonUnit
 import org.jacodb.analysis.ifds.UnitResolver
+import org.jacodb.analysis.taint.ForwardTaintFlowFunctions
 import org.jacodb.analysis.taint.TaintManager
-import org.jacodb.api.jvm.JcMethod
 import org.jacodb.panda.dynamic.api.PandaApplicationGraphImpl
+import org.jacodb.panda.dynamic.api.PandaInst
 import org.jacodb.panda.dynamic.api.PandaMethod
+import org.jacodb.panda.dynamic.api.PandaProject
 import org.jacodb.panda.dynamic.parser.ByteCodeParser
 import org.jacodb.panda.dynamic.parser.IRParser
 import org.jacodb.taint.configuration.Argument
 import org.jacodb.taint.configuration.AssignMark
 import org.jacodb.taint.configuration.ConstantTrue
 import org.jacodb.taint.configuration.ContainsMark
+import org.jacodb.taint.configuration.CopyAllMarks
+import org.jacodb.taint.configuration.RemoveMark
 import org.jacodb.taint.configuration.Result
+import org.jacodb.taint.configuration.TaintConfigurationItem
 import org.jacodb.taint.configuration.TaintMark
 import org.jacodb.taint.configuration.TaintMethodSink
 import org.jacodb.taint.configuration.TaintMethodSource
+import org.jacodb.taint.configuration.TaintPassThrough
 import org.jacodb.testing.BaseTest
 import org.jacodb.testing.WithDB
 import java.io.FileInputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 private val logger = mu.KotlinLogging.logger {}
 
@@ -46,21 +53,24 @@ class IfdsPandaTest : BaseTest() {
 
     companion object : WithDB()
 
-    private val bcFilePath = javaClass.getResource("/samples/ProgramByteCode.abc")?.path ?: ""
-    private val bytes = FileInputStream(bcFilePath).readBytes()
-    private val buffer = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN)
-    private val bcParser = ByteCodeParser(buffer)
+    private fun loadProjectForProgram(programName: String): PandaProject {
+        val bcFilePath = javaClass.getResource("/samples/${programName}.abc")?.path ?: ""
+        val bytes = FileInputStream(bcFilePath).readBytes()
+        val buffer = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN)
+        val bcParser = ByteCodeParser(buffer)
 
-    init {
         bcParser.parseABC()
+
+        val sampleFilePath = javaClass.getResource("/samples/${programName}.json")?.path ?: ""
+        val parser = IRParser(sampleFilePath, bcParser)
+        val project = parser.getProject()
+
+        return project
     }
 
-    private val sampleFilePath = javaClass.getResource("/samples/ProgramIR.json")?.path ?: ""
-    private val parser = IRParser(sampleFilePath, bcParser)
-    private val project = parser.getProject()
-
     @Test
-    fun test1() {
+    fun `test Program1`() {
+        val project = loadProjectForProgram("Program1")
         val methods = project.classes.flatMap { it.methods }
         logger.info { "Methods: ${methods.size}" }
         for (method in methods) {
@@ -68,34 +78,94 @@ class IfdsPandaTest : BaseTest() {
         }
         val graph = PandaApplicationGraphImpl(project)
         val unitResolver = UnitResolver<PandaMethod> { SingletonUnit }
-        val manager = TaintManager(graph, unitResolver) { method ->
-            val rules = buildList {
-                if (method.name == "add") {
-                    add(
+        val getConfigForMethod: ForwardTaintFlowFunctions<PandaMethod, PandaInst>.(PandaMethod) -> List<TaintConfigurationItem>? =
+            { method ->
+                val rules = buildList {
+                    if (method.name == "add") add(
                         TaintMethodSource(
-                            method = mockk<JcMethod>(),
+                            method = mockk(),
                             condition = ConstantTrue,
                             actionsAfter = listOf(
                                 AssignMark(mark = TaintMark("TAINT"), position = Result),
                             ),
                         )
                     )
-                }
-                if (method.name == "log") {
-                    add(
+                    if (method.name == "log") add(
                         TaintMethodSink(
-                            method = mockk<JcMethod>(),
+                            method = mockk(),
                             ruleNote = "CUSTOM SINK", // FIXME
                             cwe = listOf(), // FIXME
                             condition = ContainsMark(position = Argument(0), mark = TaintMark("TAINT"))
                         )
                     )
                 }
+                rules.ifEmpty { null }
             }
-            rules.ifEmpty { null }
-        }
+        val manager = TaintManager(graph, unitResolver, getConfigForMethod = getConfigForMethod)
         val sinks = manager.analyze(methods)
         logger.info { "Sinks: $sinks" }
+        assertTrue(sinks.isNotEmpty())
+    }
+
+    @Test
+    fun `test Program2`() {
+        val project = loadProjectForProgram("Program2")
+        val graph = PandaApplicationGraphImpl(project)
+        val unitResolver = UnitResolver<PandaMethod> { SingletonUnit }
+        val getConfigForMethod: ForwardTaintFlowFunctions<PandaMethod, PandaInst>.(PandaMethod) -> List<TaintConfigurationItem>? =
+            { method ->
+                val rules = buildList {
+                    if (method.name == "source") add(
+                        TaintMethodSource(
+                            method = mockk(),
+                            condition = ConstantTrue,
+                            actionsAfter = listOf(
+                                AssignMark(mark = TaintMark("TAINT"), position = Result),
+                            ),
+                        )
+                    )
+                    if (method.name == "sink") add(
+                        TaintMethodSink(
+                            method = mockk(),
+                            ruleNote = "SINK", // FIXME
+                            cwe = listOf(), // FIXME
+                            condition = ContainsMark(position = Argument(0), mark = TaintMark("TAINT"))
+                        )
+                    )
+                    if (method.name == "pass") add(
+                        TaintPassThrough(
+                            method = mockk(),
+                            condition = ConstantTrue,
+                            actionsAfter = listOf(
+                                CopyAllMarks(from = Argument(0), to = Result)
+                            ),
+                        )
+                    )
+                    if (method.name == "validate") add(
+                        TaintPassThrough(
+                            method = mockk(),
+                            condition = ConstantTrue,
+                            actionsAfter = listOf(
+                                RemoveMark(mark = TaintMark("TAINT"), position = Argument(0))
+                            ),
+                        )
+                    )
+                }
+                rules.ifEmpty { null }
+            }
+        val manager = TaintManager(graph, unitResolver, getConfigForMethod = getConfigForMethod)
+
+        val goodMethod = project.classes.flatMap { it.methods }.single { it.name == "good" }
+        logger.info { "good() method: $goodMethod" }
+        val goodSinks = manager.analyze(listOf(goodMethod))
+        logger.info { "Sinks in good(): $goodSinks" }
+        assertTrue(goodSinks.isEmpty())
+
+        val badMethod = project.classes.flatMap { it.methods }.single { it.name == "bad" }
+        logger.info { "bad() method: $badMethod" }
+        val badSinks = manager.analyze(listOf(badMethod))
+        logger.info { "Sinks in bad(): $badSinks" }
+        assertTrue(badSinks.isNotEmpty())
     }
 
 }
