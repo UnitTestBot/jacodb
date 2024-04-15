1ddcba94c23a282b349b40d46f288c016c713208
Sergey Pospelov <sergeypospelov59@gmail.com>
2024-04-15 16:06:52 +0300

    [ifds] fix: remove foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "jacodb"
 plugins {
     `gradle-enterprise`
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.11"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
 gradleEnterprise {
