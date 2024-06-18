f8387e7e8a0b41be67466c16868736ca4947424d
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2024-06-18 16:04:47 +0300

    Implement ArkApplicationGraph

--- jacodb-panda-dynamic/src/main/kotlin/org/jacodb/panda/dynamic/ark/graph/ApplicationGraph.kt
+++ jacodb-panda-dynamic/src/main/kotlin/org/jacodb/panda/dynamic/ark/graph/ApplicationGraph.kt
@@ -16,6 +16,52 @@
 
 package org.jacodb.panda.dynamic.ark.graph
 
-// class ArkApplicationGraph: ApplicationGraph<ArkMethod, Stmt> {
-//
-// }
+import org.jacodb.api.common.analysis.ApplicationGraph
+import org.jacodb.panda.dynamic.ark.base.CallStmt
+import org.jacodb.panda.dynamic.ark.base.Stmt
+import org.jacodb.panda.dynamic.ark.model.ArkFile
+import org.jacodb.panda.dynamic.ark.model.ArkMethod
+
+class ArkApplicationGraph(
+    override val project: ArkFile
+) : ApplicationGraph<ArkMethod, Stmt> {
+
+    override fun predecessors(node: Stmt): Sequence<Stmt> {
+        val graph = node.location.method.flowGraph()
+        val predecessors = graph.predecessors(node)
+        return predecessors.asSequence()
+    }
+
+    override fun successors(node: Stmt): Sequence<Stmt> {
+        val graph = node.location.method.flowGraph()
+        val successors = graph.successors(node)
+        return successors.asSequence()
+    }
+
+    override fun callees(node: Stmt): Sequence<ArkMethod> {
+        val expr = (node as CallStmt).expr
+        val method = expr.method
+        val result = project.classes.asSequence().flatMap { it.methods }.filter { it.name == method.name }
+        return result
+    }
+
+    override fun callers(method: ArkMethod): Sequence<Stmt> {
+        val result = project.classes.asSequence()
+            .flatMap { it.methods }
+            .flatMap { it.cfg.instructions }
+            .filter { stmt -> stmt is CallStmt && stmt.expr.method == method.signature }
+        return result
+    }
+
+    override fun entryPoints(method: ArkMethod): Sequence<Stmt> {
+        return method.flowGraph().entries.asSequence()
+    }
+
+    override fun exitPoints(method: ArkMethod): Sequence<Stmt> {
+        return method.flowGraph().exits.asSequence()
+    }
+
+    override fun methodOf(node: Stmt): ArkMethod {
+        return node.location.method
+    }
+}
