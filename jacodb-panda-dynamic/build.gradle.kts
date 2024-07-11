7964b8dd110daecb6307e21048501a5e62d78d7b
MForest7 <goalkeeper14631@gmail.com>
2024-07-11 14:19:28 +0300

    Add task to build arkir from sources

--- jacodb-panda-dynamic/build.gradle.kts
+++ jacodb-panda-dynamic/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.concurrent.TimeoutException
+import kotlin.time.measureTime
+
 plugins {
     kotlin("plugin.serialization")
 }
@@ -14,4 +17,29 @@ dependencies {
     testImplementation(project(":jacodb-analysis"))
     testImplementation(testFixtures(project(":jacodb-core")))
     testImplementation(Libs.mockk)
+}
+
+
+tasks.register("buildTestResources") {
+    doLast {
+        val ENV_VAR_NAME = "ARKANALYZER_DIR"
+        val arkAnalyzerDir =
+            System.getenv(ENV_VAR_NAME)
+                ?: error("Please, set $ENV_VAR_NAME variable")
+        val scriptPath = "$arkAnalyzerDir/scripts/convertAllTsToJson.ts"
+        val resourcesDir = layout.projectDirectory.dir("src/test/resources")
+        val sourceDir = resourcesDir.dir("source")
+        val irDir = resourcesDir.dir("autoir")
+        println("Building test resources for '$sourceDir' into '$irDir'...")
+        val cmd = "npx ts-node $scriptPath ${sourceDir.asFile.absolutePath} ${irDir.asFile.absolutePath}"
+        println("Running: '$cmd'...")
+        val worker = Runtime.getRuntime().exec(cmd)
+        if (!worker.waitFor(2, TimeUnit.MINUTES)) {
+            worker.destroy()
+            throw TimeoutException("Build cancelled")
+        }
+        println("Stdout: ${worker.inputStream.bufferedReader().readText()}")
+        println("Stderr: ${worker.errorStream.bufferedReader().readText()}")
+        println("Finished building test resources into '$irDir'")
+    }
 }
