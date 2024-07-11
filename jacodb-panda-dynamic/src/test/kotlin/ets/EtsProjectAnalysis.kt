4048636e7032e5ff0e9783132517e10b1d1302bd
Vladislav Kasimov <vladislavkasimov@gmail.com>
2024-07-11 19:38:42 +0300

    Remove redundant test cases and all json files

--- jacodb-panda-dynamic/src/test/kotlin/ets/EtsProjectAnalysis.kt
+++ jacodb-panda-dynamic/src/test/kotlin/ets/EtsProjectAnalysis.kt
@@ -48,7 +48,7 @@ class EtsProjectAnalysis {
     private var totalSinks: MutableList<TaintVulnerability<EtsStmt>> = mutableListOf()
 
     companion object : EtsTraits {
-        private const val SOURCE_PROJECT_PATH = "/project1"
+        private const val SOURCE_PROJECT_PATH = "/source/project1"
         private const val START_PATH = "/entry/src/main/ets/"
         const val PROJECT_PATH = "/etsir/project1"
         const val BASE_PATH = PROJECT_PATH + START_PATH
--- jacodb-panda-dynamic/src/test/resources/samples/loop/NestedLoopSimple.ts
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-        let a = 0
-    }
-}
