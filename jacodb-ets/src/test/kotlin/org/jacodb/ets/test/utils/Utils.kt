b89d197ed39f39417481d864565787103c6e91b0
Konstantin Chukharev <lipen00@gmail.com>
2024-09-25 19:54:35 +0300

    Add getResourcePathOrNull

--- jacodb-ets/src/test/kotlin/org/jacodb/ets/test/utils/Utils.kt
+++ jacodb-ets/src/test/kotlin/org/jacodb/ets/test/utils/Utils.kt
@@ -20,10 +20,13 @@ import java.io.InputStream
 import java.nio.file.Path
 import kotlin.io.path.toPath
 
-fun getResourcePath(res: String): Path {
+fun getResourcePathOrNull(res: String): Path? {
     require(res.startsWith("/")) { "Resource path must start with '/': '$res'" }
     return object {}::class.java.getResource(res)?.toURI()?.toPath()
-        ?: error("Resource not found: '$res'")
+}
+
+fun getResourcePath(res: String): Path {
+    return getResourcePathOrNull(res) ?: error("Resource not found: '$res'")
 }
 
 fun getResourceStream(res: String): InputStream {
