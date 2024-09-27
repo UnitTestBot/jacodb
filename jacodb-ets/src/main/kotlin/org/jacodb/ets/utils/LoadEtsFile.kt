eba04add679acf37c484075ed2c5536921927aa4
Alexey Menshutin <alex.menshutin99@gmail.com>
2024-09-27 16:04:33 +0300

    Type inference hacks and improvements

--- jacodb-ets/src/main/kotlin/org/jacodb/ets/utils/LoadEtsFile.kt
+++ jacodb-ets/src/main/kotlin/org/jacodb/ets/utils/LoadEtsFile.kt
@@ -19,14 +19,18 @@ package org.jacodb.ets.utils
 import org.jacodb.ets.dto.EtsFileDto
 import org.jacodb.ets.dto.convertToEtsFile
 import org.jacodb.ets.model.EtsFile
+import org.jacodb.ets.model.EtsScene
 import java.io.FileNotFoundException
 import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.Path
 import kotlin.io.path.absolute
+import kotlin.io.path.createTempDirectory
 import kotlin.io.path.exists
 import kotlin.io.path.inputStream
 import kotlin.io.path.nameWithoutExtension
 import kotlin.io.path.pathString
+import kotlin.io.path.walk
 import kotlin.time.Duration.Companion.seconds
 
 private const val ENV_VAR_ARK_ANALYZER_DIR = "ARKANALYZER_DIR"
@@ -38,14 +42,14 @@ private const val DEFAULT_SERIALIZE_SCRIPT_PATH = "out/src/save/serializeArkIR.j
 private const val ENV_VAR_NODE_EXECUTABLE = "NODE_EXECUTABLE"
 private const val DEFAULT_NODE_EXECUTABLE = "node"
 
-fun generateEtsFileIR(tsPath: Path): Path {
+fun generateEtsIR(path: Path, isProject: Boolean = false): Path {
     val arkAnalyzerDir = Path(System.getenv(ENV_VAR_ARK_ANALYZER_DIR) ?: DEFAULT_ARK_ANALYZER_DIR)
     if (!arkAnalyzerDir.exists()) {
         throw FileNotFoundException(
             "ArkAnalyzer directory does not exist: '${arkAnalyzerDir.absolute()}'. " +
-                "Did you forget to set the '$ENV_VAR_ARK_ANALYZER_DIR' environment variable? " +
-                "Current value is '${System.getenv(ENV_VAR_ARK_ANALYZER_DIR)}', " +
-                "current dir is '${Path("").toAbsolutePath()}'."
+                    "Did you forget to set the '$ENV_VAR_ARK_ANALYZER_DIR' environment variable? " +
+                    "Current value is '${System.getenv(ENV_VAR_ARK_ANALYZER_DIR)}', " +
+                    "current dir is '${Path("").toAbsolutePath()}'."
         )
     }
 
@@ -54,27 +58,49 @@ fun generateEtsFileIR(tsPath: Path): Path {
     if (!script.exists()) {
         throw FileNotFoundException(
             "Script file not found: '$script'. " +
-                "Did you forget to execute 'npm run build' in the arkanalyzer project?"
+                    "Did you forget to execute 'npm run build' in the arkanalyzer project?"
         )
     }
 
     val node = System.getenv(ENV_VAR_NODE_EXECUTABLE) ?: DEFAULT_NODE_EXECUTABLE
-    val output = kotlin.io.path.createTempFile(prefix = tsPath.nameWithoutExtension, suffix = ".json")
-    val cmd: List<String> = listOf(
+    val output = if (isProject) {
+        createTempDirectory(prefix = path.nameWithoutExtension)
+    } else {
+        kotlin.io.path.createTempFile(prefix = path.nameWithoutExtension, suffix = ".json")
+    }
+
+    val cmd = listOfNotNull(
         node,
         script.pathString,
-        tsPath.pathString,
+        if (isProject) "-p" else null,
+        path.pathString,
         output.pathString,
     )
-    runProcess(cmd, 60.seconds)
+    runProcess(cmd, 10.seconds)
     return output
 }
 
-fun loadEtsFileAutoConvert(tsPath: Path): EtsFile {
-    val irFilePath = generateEtsFileIR(tsPath)
+fun loadEtsFileAutoConvert(path: Path): EtsFile {
+    val irFilePath = generateEtsIR(path, isProject = false)
+
     irFilePath.inputStream().use { stream ->
         val etsFileDto = EtsFileDto.loadFromJson(stream)
         val etsFile = convertToEtsFile(etsFileDto)
         return etsFile
     }
+}
+
+@OptIn(ExperimentalPathApi::class)
+fun loadEtsProjectAutoConvert(path: Path): EtsScene {
+    val irFolderPath = generateEtsIR(path, isProject = true)
+
+    val files = irFolderPath
+        .walk()
+        .filter { it.toFile().extension == "json" }
+        .mapTo(mutableListOf()) {
+            val etsFileDto = EtsFileDto.loadFromJson(it.inputStream())
+            convertToEtsFile(etsFileDto)
+        }
+
+    return EtsScene(files)
 }
