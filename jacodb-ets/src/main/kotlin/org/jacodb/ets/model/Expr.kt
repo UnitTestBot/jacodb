d138ae658933a8f006f75fcdf6f20d41fdb1dd8d
Konstantin Chukharev <lipen00@gmail.com>
2025-07-18 16:54:34 +0300

    Add "virtual"/"static"/"ptr" to CallExpr toString

--- jacodb-ets/src/main/kotlin/org/jacodb/ets/model/Expr.kt
+++ jacodb-ets/src/main/kotlin/org/jacodb/ets/model/Expr.kt
@@ -736,7 +736,7 @@ data class EtsInstanceCallExpr(
     override val type: EtsType,
 ) : EtsCallExpr, CommonInstanceCallExpr {
     override fun toString(): String {
-        return "$instance.${callee.name}(${args.joinToString()})"
+        return "virtual ${instance}.${callee.name}(${args.joinToString()})"
     }
 
     override fun <R> accept(visitor: EtsExpr.Visitor<R>): R {
@@ -750,7 +750,7 @@ data class EtsStaticCallExpr(
     override val type: EtsType,
 ) : EtsCallExpr {
     override fun toString(): String {
-        return "${callee.enclosingClass.name}.${callee.name}(${args.joinToString()})"
+        return "static ${callee.enclosingClass.name}.${callee.name}(${args.joinToString()})"
     }
 
     override fun <R> accept(visitor: EtsExpr.Visitor<R>): R {
@@ -765,7 +765,7 @@ data class EtsPtrCallExpr(
     override val type: EtsType,
 ) : EtsCallExpr {
     override fun toString(): String {
-        return "${ptr}<${callee.name}>(${args.joinToString()})"
+        return "ptr ${ptr}(${args.joinToString()})"
     }
 
     override fun <R> accept(visitor: EtsExpr.Visitor<R>): R {
