8d04263dd68f8abaa048d47d084bdca9114c15ed
Konstantin Chukharev <lipen00@gmail.com>
2025-10-30 17:28:01 +0300

    Fix BFS sample

--- jacodb-ets/src/test/resources/samples/source/loop/BreadthFirstSearch.ts
+++ jacodb-ets/src/test/resources/samples/source/loop/BreadthFirstSearch.ts
@@ -3,23 +3,25 @@ interface Graph {
 }
 
 function bfs(graph: Graph, startNode: string): string[] {
-    const visited = [];
+    const visited = new Set<string>();
     const queue = [startNode];
+    const order = [];
 
     while (queue.length > 0) {
         const node = queue.shift();
-        if (!visited.includes(node)) {
-            visited.push(node);
+        if (!visited.has(node)) {
+            visited.add(node);
+            order.push(node);
             const neighbours = graph[node];
             for (const neighbour of neighbours) {
-                if (!visited.includes(neighbour)) {
+                if (!visited.has(neighbour)) {
                     queue.push(neighbour);
                 }
             }
         }
     }
 
-    return visited;
+    return order;
 }
 
 const graph: Graph = {
@@ -28,8 +30,8 @@ const graph: Graph = {
     c: ['e'],
     d: ['f'],
     e: [],
-    f: []
+    f: [],
 };
 
 const visitOrder = bfs(graph, 'a');
-console.log('Visit order:', visitOrder);
+console.log('Visit order:', visitOrder); // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
